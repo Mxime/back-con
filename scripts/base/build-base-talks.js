5a5c4fb7b1cd4b42c0d113b719e2203dd6ef7312
Maxime <maxime.martin@backmarket.com>
2024-05-14 12:56:56 +0200

    feat: add base script to build the base folders for talks

---
+++ scripts/base/build-base-talks.js
@@ -0,0 +1,37 @@
+const fs = require("node:fs");
+const talks = require("./talks.json");
+
+talks.forEach((talk) => {
+  const talkFolderPath = `../../public/talks/${talk.title}`;
+
+  if (!fs.existsSync(talkFolderPath)) {
+    // create a folder in public/talks
+    fs.mkdirSync(talkFolderPath);
+  }
+
+  if (!fs.existsSync(`${talkFolderPath}/index.json`)) {
+    const indexFile = {
+      title: `TO UPDATE: ${talk.title}`,
+      category: talk.category,
+      location: talk.location || "",
+      time: talk.time || "2024-06-18T11:00:00Z",
+      speakers: talk.speakers,
+    };
+
+    // write index.json file
+    fs.writeFileSync(
+      `${talkFolderPath}/index.json`,
+      JSON.stringify(indexFile),
+      null,
+      4
+    );
+  }
+
+  if (!fs.existsSync(`${talkFolderPath}/description.md`)) {
+    // write description.markdown file
+    fs.writeFileSync(
+      `${talkFolderPath}/description.md`,
+      "Description to **update**"
+    );
+  }
+});
